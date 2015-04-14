a94bc333f314e8ea78f71fdc31021c8ad5ba86b5
tpetchel <tpetchel@gmail.com>
2015-04-14 14:57:18 -0700

    Initial commit

---
+++ chef-repo/cookbooks/web_application/metadata.rb
@@ -0,0 +1,10 @@
+name             'web_application'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures web_application'
+long_description 'Installs/Configures web_application'
+version          '0.3.0'
+
+depends 'windows', '~> 1.36.6'
+depends 'iis', '~> 4.1.0'
