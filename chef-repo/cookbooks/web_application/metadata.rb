f2c71092c0774ee8c877b66a4fb82285e3fe73f4
tpetchel <tpetchel@gmail.com>
2015-08-21 07:37:52 -0400

    final tutorial code
    * renamed web_application -> awesome_customers

--- chef-repo/cookbooks/web_application/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'web_application'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
-license          'all_rights'
-description      'Installs/Configures web_application'
-long_description 'Installs/Configures web_application'
-version          '0.1.0'
-
-depends 'windows', '~> 1.36.6'
